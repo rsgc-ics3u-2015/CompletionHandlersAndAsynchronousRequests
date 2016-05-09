9316cb902eba06016172eed36609afe13bef98d0
Russell Gordon <rgordon@rsgc.on.ca>
2016-05-09 15:07:45 -0400

    Comments fix

--- CompletionHandlersAndAsynchronousRequests.playground/Contents.swift
+++ CompletionHandlersAndAsynchronousRequests.playground/Contents.swift
@@ -5,11 +5,11 @@ import UIKit
 // First, define a URL to retrieve a JSON file from
 let address : String = "http://www.learnswiftonline.com/Samples/subway.json"
 
-// Try to make a URL Request
+// Try to make a URL request object
 if let url = NSURL(string: address) {
     
-    // We have an object of type NSURL to work with
-    print(url)
+    // We have an valid URL to work with
+    print(url)    
     
 } else {
     
